5201fa680c648603364f8515fe5d1a9cf9e535a5
WhizzyDoc <hassanridwan2536@gmail.com>
2022-09-16 22:22:42 +0100

    a

--- src/script.js
+++ src/script.js
@@ -1,5 +1,12 @@
 var subs = document.querySelector(".subscribe-newsletters");
 var nav = document.querySelector(".navigation");
+var local = document.querySelectorAll(".local");
+var inter = document.querySelectorAll(".inter");
+var trend = document.querySelectorAll(".trend");
+var locala = document.querySelector("#local");
+var intera = document.querySelector("#inter");
+var trenda = document.querySelector("#trend");
+var alla = document.querySelector("#all");
 
 function subscribe() {
     subs.style.display = "flex";
@@ -14,4 +21,66 @@ function opennav() {
 function closenav() {
     nav.style.width = "0px";
     nav.style.paddingLeft = "0px";
+}
+
+
+function showAll() {
+    alla.classList.add("active");
+    intera.classList.remove("active");
+    trenda.classList.remove("active");
+    locala.classList.remove("active");
+    for(var i=0; i<trend.length; i++) {
+        trend[i].style.display = "block";
+    }
+    for(var i=0; i<local.length; i++) {
+        local[i].style.display = "block";
+    }
+    for(var i=0; i<inter.length; i++) {
+        inter[i].style.display = "block";
+    }
+}
+function showLocal() {
+    locala.classList.add("active");
+    intera.classList.remove("active");
+    trenda.classList.remove("active");
+    alla.classList.remove("active");
+    for(var i=0; i<trend.length; i++) {
+        trend[i].style.display = "none";
+    }
+    for(var i=0; i<inter.length; i++) {
+        inter[i].style.display = "none";
+    }
+    for(var i=0; i<local.length; i++) {
+        local[i].style.display = "block";
+    }
+}
+function showInter() {
+    intera.classList.add("active");
+    locala.classList.remove("active");
+    trenda.classList.remove("active");
+    alla.classList.remove("active");
+    for(var i=0; i<trend.length; i++) {
+        trend[i].style.display = "none";
+    }
+    for(var i=0; i<local.length; i++) {
+        local[i].style.display = "none";
+    }
+    for(var i=0; i<inter.length; i++) {
+        inter[i].style.display = "block";
+    }
+}
+function showTrend() {
+    trenda.classList.add("active");
+    intera.classList.remove("active");
+    locala.classList.remove("active");
+    alla.classList.remove("active");
+    for(var i=0; i<local.length; i++) {
+        local[i].style.display = "none";
+    }
+    for(var i=0; i<inter.length; i++) {
+        inter[i].style.display = "none";
+    }
+    for(var i=0; i<trend.length; i++) {
+        trend[i].style.display = "block";
+    }
 }
